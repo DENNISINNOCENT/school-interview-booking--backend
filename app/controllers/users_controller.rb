88f3966ffcb891ba21b7f739bf2f274298320536
DENNISINNOCENT <mutindahdennis@gmail.com>
2023-02-01 11:32:16 +0300

    status code for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
   def index
 
-    render json: User.all
+    render json: User.all, status: :ok
   end
 
   def create
     user = User.create(user_params)
-    render json: user
+    render json: user, status: :created
   end
 
   def show
     user = find_user
-    render json: user
+    render json: user, status: :ok
   end
   def update
     user = find_user
     user.update(user_params)
-    render json: user
+    render json: user, status: :accepted
   end
   def destroy
     user = find_user
